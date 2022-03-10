bb7e9edb27fb7a1752ad33563a7dd62135b1a94c
mazoSant <santiago.mazo2@udea.edu.co>
2022-03-10 17:13:21 -0500

    Here I completed the game but I dont know why I have an error, I think is the for loop

--- hangman_game.py
+++ hangman_game.py
@@ -3,7 +3,7 @@ import random
 words={}
 numero_random=random.randint(0,171)
 def read_data():
-        with open(r"C:\Users\EQUIPO\documents\juego_del_ahorcado\data.txt", encoding="utf-8") as f:
+        with open(r"C:\Users\EQUIPO\documents\PROYECTOS_PERSONALES\juego_del_ahorcado\data.txt", encoding="utf-8") as f:
             words=[word.strip('\n') for word in f] #Here eliminated the espaces (Saltos de linea) y ennumere las palabras en el diccionario
             words=dict(enumerate(words))
             return words.get(numero_random)
@@ -17,7 +17,23 @@ def normalize(s):
         for a, b in replacements:
             s = s.replace(a, b).replace(a.upper(), b.upper())
         return s
+def run():
+    palabra_adivinar=normalize(read_data())
+    palabra_adivinando= "_"*len(normalize(read_data()))
+    while palabra_adivinar != palabra_adivinando:
+        print(palabra_adivinando)
+        lyrics=str(input("Ingrese una letra: "))
+        if lyrics in palabra_adivinar:
+            palabra_adivinando=list(palabra_adivinando)
+            for i,x in enumerate(palabra_adivinar):
+                if x==lyrics:
+                    palabra_adivinando[i]=x
+                    palabra_adivinando="".join(palabra_adivinando)
+    print("Felicitaciones GANASTE")
+                    
+    
 
-print(normalize(read_data()))
+run()
+
 
 
