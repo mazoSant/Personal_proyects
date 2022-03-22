70a1d3f0706f275ac7655420329e552d28fc039b
mazoSant <santiago.mazo2@udea.edu.co>
2022-03-22 01:09:16 -0500

    Here I added a life counter and improve the game

--- hangman_game.py
+++ hangman_game.py
@@ -22,10 +22,13 @@ def normalize(s):
 def run():
     palabra_adivinar=normalize(read_data())
     palabra_adivinando= "_"*len(normalize(read_data()))
-    
-    while palabra_adivinar != palabra_adivinando:
-        print(palabra_adivinando)
+    contador_vidas=0
+    numero_vidas=len(normalize(read_data()))+5
+    print("Tienes" , numero_vidas ," vidas")
+    while palabra_adivinar != palabra_adivinando and contador_vidas!=numero_vidas:
+        print(palabra_adivinando.upper())
         lyrics=str(input("Ingrese una letra: "))
+        
         if lyrics in palabra_adivinar:
             palabra_adivinando=list(palabra_adivinando)
             for i,x in enumerate(palabra_adivinar):
@@ -33,13 +36,18 @@ def run():
                     palabra_adivinando[i]=x
             palabra_adivinando="".join(palabra_adivinando)      
         os.system('cls')
-        if lyrics not in palabra_adivinar:
+        
+        if lyrics not in palabra_adivinar and contador_vidas!=numero_vidas:
+            contador_vidas+=1
             print("Prueba con otra letra")
-               
-    print("Felicitaciones GANASTE, la palabra era: "+ palabra_adivinando.upper())
+        print("Te quedan: " ,(numero_vidas - contador_vidas), " vidas")
+    if palabra_adivinar== palabra_adivinando:           
+        print("Felicitaciones GANASTE, la palabra era: "+ palabra_adivinando.upper())
+    else:
+        print("Perdiste, se te acabaron las vidas \n La palabra era : "+ palabra_adivinar.upper())
                   
 if __name__=='__main__':   
- run()
+    run()
 
 
 
